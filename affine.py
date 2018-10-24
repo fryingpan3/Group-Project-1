745f8b981a7d9384676d2c8a2acb4f9c4120403f
Mark <44260176+MarkBre@users.noreply.github.com>
2018-10-24 14:22:26 +0100

    Update affine.py

--- affine.py
+++ affine.py
@@ -6,30 +6,41 @@ def Affine_En(Key1Input, Key2Input, plainText):
     planetext = plainText
     numtext = []
     newnum = []
-    alph = ["a","b","c","d","e","f","g","h","i","j","k","m","n","o","p","q","r","s","t","u","v","w","x","y","z","!"]
+    alph = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
     # convert to num
     for i in range (0, len(planetext)):
-        for p in range (0,25):
-            if alph[int(p)] == planetext[int(i)]:
+        for p in range (0,27):
+            print(planetext[int(i)], p)
+            if p == 26:
+                numtext.append(planetext[int(i)])
+                print("planetext")
+                
+            elif alph[int(p)] == planetext[int(i)]:
                 numtext.append(p)
+                break
+            
+    print(numtext)
     # apply cypher
     for i in range (0, len(numtext)):
         try:
-            numtext[i] = (Akey*numtext[i]+Bkey) % 27
+            numtext[i] = (Akey*numtext[i]+Bkey) % 26
         except:
-            print("")
+            print("except cypher")
     # covert back to letters
     for i in range(0, len(numtext)):
         try:
             EnString.append(alph[numtext[i]])
         except:
-            number = numtext[i] - 26
-            EnString.append(alph[number])
+            try:
+                number = numtext[i] - 26
+                EnString.append(alph[number])
+            except:
+                EnString.append(numtext[i])
+                
     String = planetext
     "".join(EnString)
     print(EnString)
     return EnString
-
 
             
         
